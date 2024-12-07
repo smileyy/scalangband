b8c5f35bfdd21bd94e0adb5c4d48a87e1ff4ee86
smileyy <smileyy@gmail.com>
2024-12-07 15:22:10 -0800

    oops: add garbage item generation back to townsperson (#55)

--- main/scala/scalangband/model/monster/person/RandomlyMumblingTownsperson.scala
+++ main/scala/scalangband/model/monster/person/RandomlyMumblingTownsperson.scala
@@ -2,6 +2,7 @@ package scalangband.model.monster.person
 
 import scalangband.model.action.monster.{MonsterAction, MonsterPassAction, RandomMovementAction, TauntAction}
 import scalangband.model.item.ItemGenerator
+import scalangband.model.item.garbage.GarbageGenerator
 import scalangband.model.item.money.MoneyGenerator
 import scalangband.model.monster.{MonsterFactory, MonsterInventoryGenerator, MonsterSpec, Person}
 import scalangband.model.util.{DiceRoll, Weighted}
@@ -28,7 +29,7 @@ object RandomlyMumblingTownsperson extends MonsterFactory {
     new MonsterInventoryGenerator(1, 2, generators)
 
   def generators: Seq[Weighted[ItemGenerator]] = Seq(
-//    Weighted(GarbageGenerator, 1),
+    Weighted(GarbageGenerator, 1),
     Weighted(MoneyGenerator, 1)
   )
 }
