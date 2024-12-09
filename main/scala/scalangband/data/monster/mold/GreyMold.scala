5e841327e9fa056caacff217cc52a5ed942322a0
smileyy <smileyy@gmail.com>
2024-12-09 14:28:21 -0800

    81: Grey Mold spore attack (#82)

--- main/scala/scalangband/data/monster/mold/GreyMold.scala
+++ main/scala/scalangband/data/monster/mold/GreyMold.scala
@@ -1,7 +1,8 @@
 package scalangband.data.monster.mold
 
 import scalangband.bridge.rendering.TextColors
-import scalangband.model.monster.action.MonsterPassAction
+import scalangband.model.monster.action.{AdjacentToPlayerActions, MeleeAttacksAction, MonsterAction, MonsterPassAction}
+import scalangband.model.monster.attack.SporeAttack
 import scalangband.model.monster.{Mold, MonsterFactory, MonsterSpec}
 import scalangband.model.util.{DiceRoll, Weighted}
 
@@ -12,7 +13,18 @@ object GreyMold extends MonsterFactory {
     level = 1,
     health = DiceRoll("1d3"),
     armorClass = 1,
-    actions = Seq(Weighted(1, MonsterPassAction)),
+    actions = actions,
     color = TextColors.MediumGrey
   )
+
+  private def actions: Seq[Weighted[MonsterAction]] = Seq(
+    Weighted(100, AdjacentToPlayerActions(
+      adjacent = Seq(Weighted(100, MeleeAttacksAction(Seq(
+        new SporeAttack(DiceRoll("1d4")),
+        new SporeAttack(DiceRoll("1d4"))
+      )))),
+      otherwise = Seq(Weighted(100, MonsterPassAction))
+    ))
+  )
+
 }
--- main/scala/scalangband/model/monster/attack/MeleeAttacks.scala
+++ main/scala/scalangband/model/monster/attack/MeleeAttacks.scala
@@ -3,17 +3,25 @@ package scalangband.model.monster.attack
 import scalangband.model.monster.Monster
 import scalangband.model.util.DiceRoll
 
-class BasicAttack(val damage: DiceRoll) extends MeleeAttack {
+class BlowAttack(val damage: DiceRoll) extends MeleeAttack {
   override def power: Int = 40
 }
 
-class CrawlAttack(dmg: DiceRoll) extends BasicAttack(dmg) {
+class CrawlAttack(dmg: DiceRoll) extends BlowAttack(dmg) {
   override def hitMessage(monster: Monster): Option[String] = {
     Some(s"The ${monster.displayName} crawls on you.")
   }
 }
 
-class StingAttack(dmg: DiceRoll) extends BasicAttack(dmg) {
+class SporeAttack(dmg: DiceRoll) extends BlowAttack(dmg) {
+  override def hitMessage(monster: Monster): Option[String] = {
+    Some(s"The ${monster.displayName} releases spores at you.")
+  }
+
+  override def missMessage(monster: Monster): Option[String] = None
+}
+
+class StingAttack(dmg: DiceRoll) extends BlowAttack(dmg) {
   override def hitMessage(monster: Monster): Option[String] = {
     Some(s"The ${monster.displayName} stings you.")
   }
