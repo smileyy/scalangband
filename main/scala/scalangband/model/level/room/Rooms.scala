de063172ad294e27d9d22b370d4f589bce07a7e8
smileyy <smileyy@gmail.com>
2024-12-11 15:14:23 -0800

    93-scalafmt (#94)
    * 93: scalafmt some files
    
    * 93: scalafmt some files

--- main/scala/scalangband/model/level/room/Rooms.scala
+++ main/scala/scalangband/model/level/room/Rooms.scala
@@ -27,22 +27,19 @@ trait Room {
 
   def right: Int = left + width - 1
 
-  /**
-   * Returns a place where a hallway can be attached to enter the room. This method may throw an Exception if the room
-   * should be detached, which is a rare case. So, uh, always check that a room is supposed to be attached before
-   * checking for an attachment point.
-   */
+  /** Returns a place where a hallway can be attached to enter the room. This method may throw an Exception if the room
+    * should be detached, which is a rare case. So, uh, always check that a room is supposed to be attached before
+    * checking for an attachment point.
+    */
   def getAttachmentPoint(random: Random, direction: Direction): Coordinates
 
-  /**
-   * Replaces the tile at the given indices.
-   */
+  /** Replaces the tile at the given indices.
+    */
   def setTile(row: Int, col: Int, tile: Tile): Unit
 
-  /**
-   * Whether this room should be attached to the rest of the level. If a room is to be attached, it must provide 
-   * possible attachment points for each direction.
-   */
+  /** Whether this room should be attached to the rest of the level. If a room is to be attached, it must provide
+    * possible attachment points for each direction.
+    */
   def attached: Boolean = true
 
   def addMonsters(random: Random, bestiary: Bestiary): Unit
@@ -54,31 +51,36 @@ trait Room {
   }
 }
 
-abstract class AbstractRoom(val tiles: Array[Array[Tile]], val top: Int, val left: Int, val effectiveDepth: Int) extends Room {
+abstract class AbstractRoom(val tiles: Array[Array[Tile]], val top: Int, val left: Int, val effectiveDepth: Int)
+    extends Room {
   override def setTile(row: Int, col: Int, tile: Tile): Unit = {
     tiles(row)(col) = tile
   }
 
   override def addMonsters(random: Random, bestiary: Bestiary): Unit = {
     val numberOfMonsters = random.nextInt(100) match {
-      case x if x < 25 => 0
-      case x if x < 75 => 1
-      case x if x < 95 => 2
+      case x if x < 25  => 0
+      case x if x < 75  => 1
+      case x if x < 95  => 2
       case x if x < 100 => 3
     }
 
     (0 until numberOfMonsters).foreach(_ => {
-      val (row, col) = RandomUtils.randomPairs(random, height, width).filter((r, c) => tiles(r)(c) match {
-        case ot: OccupiableTile if !ot.occupied => true
-        case _ => false
-      }).head
+      val (row, col) = RandomUtils
+        .randomPairs(random, height, width)
+        .filter((r, c) =>
+          tiles(r)(c) match {
+            case ot: OccupiableTile if !ot.occupied => true
+            case _                                  => false
+          }
+        )
+        .head
 
       val monsterDepth: Int = LevelRanges.ranges(effectiveDepth).randomInRange(random)
 
       val monster = generateMonster(random, bestiary, monsterDepth, Coordinates(row + top, col + left))
       tiles(row)(col).asInstanceOf[OccupiableTile].setOccupant(monster)
-    }
-    )
+    })
   }
 }
 object Room {
--- main/scala/scalangband/model/player/Equipment.scala
+++ main/scala/scalangband/model/player/Equipment.scala
@@ -8,8 +8,9 @@ import scalangband.model.item.weapon.Weapon
 class Equipment(
     var weapon: Option[Weapon] = None,
     var light: Option[LightSource] = None,
-    var body: Option[BodyArmor] = None) {
-  
+    var body: Option[BodyArmor] = None
+) {
+
   def allEquipment: Seq[EquippableItem] = Seq(weapon, light, body).flatten
   override def toString: String = s"{ weapon: $weapon, light: $light, body: $body }"
 }
