74bb8b9dac4850c47d40d0b662a03ea9270a9ece
smileyy <smileyy@gmail.com>
2024-12-12 21:50:55 -0800

    91: green player pane values (#113)

--- main/scala/scalangband/ui/GamePanel.scala
+++ main/scala/scalangband/ui/GamePanel.scala
@@ -2,6 +2,7 @@ package scalangband.ui
 
 import scalangband.Scalangband
 import scalangband.bridge.actionresult.{ActionResult, MessageResult, NoResult}
+import scalangband.bridge.rendering.TextColors
 import scalangband.bridge.rendering.TextColors.*
 import scalangband.model.Game
 import scalangband.model.player.action.PlayerAction
@@ -12,10 +13,15 @@ import scalangband.ui.render.Renderer
 import scala.swing.*
 import scala.swing.event.*
 
-class GamePanel(game: Game, var renderer: Renderer, var keyHandlers: List[KeyHandler], var messages: List[String] = List.empty) extends Panel {
+class GamePanel(
+    game: Game,
+    var renderer: Renderer,
+    var keyHandlers: List[KeyHandler],
+    var messages: List[String] = List.empty
+) extends Panel {
 
   private val callback = new GamePanelCallback(this)
-  
+
   font = Font(Font.Monospaced, Font.Plain, 12)
 
   preferredSize = new Dimension(800, 600)
@@ -24,17 +30,16 @@ class GamePanel(game: Game, var renderer: Renderer, var keyHandlers: List[KeyHan
   focusable = true
   listenTo(keys)
 
-  reactions += {
-    case kp: KeyPressed =>
-      if (game.player.isDead) {
-        Scalangband.endGame()
-      }
-      if (messages.nonEmpty) {
-        repaint()
-      } else {
-        repaint()
-        messages = List.empty
-        keyHandlers.head.handleKeyPressed(kp, game, callback).foreach(action => dispatchAction(action))
+  reactions += { case kp: KeyPressed =>
+    if (game.player.isDead) {
+      Scalangband.endGame()
+    }
+    if (messages.nonEmpty) {
+      repaint()
+    } else {
+      repaint()
+      messages = List.empty
+      keyHandlers.head.handleKeyPressed(kp, game, callback).foreach(action => dispatchAction(action))
     }
   }
 
@@ -46,19 +51,20 @@ class GamePanel(game: Game, var renderer: Renderer, var keyHandlers: List[KeyHan
 
   private def applyResult(result: ActionResult): Unit = result match {
     case MessageResult(message) => this.messages = message :: this.messages
-    case NoResult =>
+    case NoResult               =>
   }
 
   override def paintComponent(g: Graphics2D): Unit = {
     super.paintComponent(g)
 
     val lineHeight = g.getFontMetrics(font).getHeight
-    val characterPaneWidth = g.getFontMetrics(font).charWidth(' ') * (PlayerPaneWidth + 1)
+    val charWidth = g.getFontMetrics(font).charWidth(' ')
+    val playerPanePixelWidth = charWidth * (PlayerPaneWidth + 1)
 
     paintMessages(g, lineHeight)
-    paintPlayer(g, lineHeight, lineHeight)
-    paintLevel(g, lineHeight, characterPaneWidth)
-    paintDepth(g, lineHeight, characterPaneWidth)
+    paintPlayer(g, charWidth, lineHeight, lineHeight)
+    paintLevel(g, lineHeight, playerPanePixelWidth + charWidth)
+    paintDepth(g, lineHeight, playerPanePixelWidth + charWidth)
   }
 
   private def paintMessages(g: Graphics2D, messageLineYOffset: Int): Unit = {
@@ -76,7 +82,7 @@ class GamePanel(game: Game, var renderer: Renderer, var keyHandlers: List[KeyHan
     }
   }
 
-  private def paintPlayer(g: Graphics2D, yOffset: Int, lineHeight: Int): Unit = {
+  private def paintPlayer(g: Graphics2D, charWidth: Int, yOffset: Int, lineHeight: Int): Unit = {
     g.setColor(White)
 
     val playerNameDisplay = if (game.player.name.length < PlayerPaneWidth) {
@@ -92,34 +98,47 @@ class GamePanel(game: Game, var renderer: Renderer, var keyHandlers: List[KeyHan
     g.drawString(game.player.cls.name, 0, yOffset + lineHeight * 3)
 
     g.setColor(White)
-    g.drawString(rightAlignedFieldString("Level", game.player.level.toString), 0, yOffset + lineHeight * 5)
-    g.drawString(rightAlignedFieldString("AU", game.player.money.toString), 0, yOffset + lineHeight * 7)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "Level", game.player.level.toString, Green, 0, yOffset + lineHeight * 5)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "AU", game.player.money.toString, Green, 0, yOffset + lineHeight * 7)
 
-    g.drawString(rightAlignedFieldString("STR:", game.player.stats.str.toString), 0, yOffset + lineHeight * 9)
-    g.drawString(rightAlignedFieldString("INT:", game.player.stats.intg.toString), 0, yOffset + lineHeight * 10)
-    g.drawString(rightAlignedFieldString("WIS:", game.player.stats.wis.toString), 0, yOffset + lineHeight * 11)
-    g.drawString(rightAlignedFieldString("DEX:", game.player.stats.dex.toString), 0, yOffset + lineHeight * 12)
-    g.drawString(rightAlignedFieldString("CON:", game.player.stats.con.toString), 0, yOffset + lineHeight * 13)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "STR:", game.player.stats.str.toString, Green, 0, yOffset + lineHeight * 9)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "INT:", game.player.stats.intg.toString, Green, 0, yOffset + lineHeight * 10)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "WIS:", game.player.stats.wis.toString, Green, 0, yOffset + lineHeight * 11)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "DEX:", game.player.stats.dex.toString, Green, 0, yOffset + lineHeight * 12)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "CON:", game.player.stats.con.toString, Green, 0, yOffset + lineHeight * 13)
 
-    g.drawString(rightAlignedFieldString("AC", game.player.armorClass.toString), 0, yOffset + lineHeight * 15)
-    g.drawString(rightAlignedFieldString("HP", game.player.health.toString), 0, yOffset + lineHeight * 16)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "AC", game.player.armorClass.toString, Green, 0, yOffset + lineHeight * 15)
+    drawLabeledValue(g, charWidth, PlayerPaneWidth, "HP", game.player.health.toString, Green, 0, yOffset + lineHeight * 16)
   }
 
-  private def rightAlignedFieldString(label: String, value: String) = {
-    val displayString = StringBuilder(label)
-    val padding = PlayerPaneWidth - displayString.length() - value.length
-    displayString.append("".padTo(padding, ' '))
-    displayString.append(value)
+  def drawLabeledValue(
+      g: Graphics2D,
+      charWidthInPixels: Int,
+      totalWidthInChars: Int,
+      label: String,
+      value: String,
+      color: Color,
+      x: Int,
+      y: Int
+  ): Unit = {
+    val valueOffset = (PlayerPaneWidth - value.length) * charWidthInPixels
 
-    displayString.toString()
+    g.setColor(White)
+    g.drawString(label, x, y)
+    g.setColor(color)
+    g.drawString(value, x + valueOffset, y)
   }
-
+  
   private def paintLevel(g: Graphics2D, messageLineHeight: Int, characterPaneWidth: Int): Unit = {
     val tiles = renderer.render(game.level)
 
     for (rowIdx <- tiles.indices) {
       for (colIdx <- tiles(rowIdx).indices) {
-        tiles(rowIdx)(colIdx).render(g, colIdx * renderer.tileWidth + characterPaneWidth, (rowIdx + 1) * renderer.tileHeight + messageLineHeight + 1)
+        tiles(rowIdx)(colIdx).render(
+          g,
+          colIdx * renderer.tileWidth + characterPaneWidth,
+          (rowIdx + 1) * renderer.tileHeight + messageLineHeight + 1
+        )
       }
     }
   }
@@ -143,7 +162,7 @@ class GamePanelCallback(panel: GamePanel) {
   def pushKeyHandler(handler: KeyHandler): Unit = {
     panel.keyHandlers = handler :: panel.keyHandlers
   }
-  
+
   def popKeyHandler(): Unit = {
     panel.keyHandlers = panel.keyHandlers.tail
   }
