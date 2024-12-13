c4848c2c85d199ed9f2f36758393cfa652c020e7
smileyy <smileyy@gmail.com>
2024-12-12 21:50:33 -0800

    91: green player pane values

--- main/scala/scalangband/bridge/rendering/TextColors.scala
+++ main/scala/scalangband/bridge/rendering/TextColors.scala
@@ -6,6 +6,7 @@ object TextColors {
   val Black = new Color(0, 0, 0)
   val Brown = new Color(165, 42, 42)
   val DarkGrey = new Color(79, 79, 79)
+  val Green = new Color(0,232,0)
   val LightBeige = new Color(237,232,208)
   val MediumGrey = new Color(95, 95, 95)
   val Red = new Color(255, 0, 0)
