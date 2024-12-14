4fc4b4e74bab40bda779a17dbad5dc4bfe2724f6
smileyy <smileyy@gmail.com>
2024-12-13 19:03:35 -0800

    100: dex toHit and toArmor (#119)

--- main/scala/scalangband/model/player/Stats.scala
+++ main/scala/scalangband/model/player/Stats.scala
@@ -1,16 +1,16 @@
 package scalangband.model.player
 
-class Stats(val str: Str, val intg: Intg, val wis: Wis, val dex: Dex, val con: Con) {
+class Stats(val str: Strength, val intg: Intelligence, val wis: Wisdom, val dex: Dexterity, val con: Constitution) {
   def allStats: Seq[Stat[?]] = Seq(str, intg, wis, dex, con)
-  
+
   def toHit: Int = allStats.map(_.toHit).sum
   def toDamage: Int = allStats.map(_.toDamage).sum
-  
+
   def +(b: StatBonuses): Stats = new Stats(str + b.str, intg + b.intg, wis + b.wis, dex + b.dex, con + b.con)
 }
 object Stats {
   def apply(str: Int, intg: Int, wis: Int, dex: Int, con: Int): Stats = {
-    new Stats(new Str(str), Intg(intg), Wis(wis), Dex(dex), Con(con))
+    new Stats(new Strength(str), Intelligence(intg), Wisdom(wis), Dexterity(dex), Constitution(con))
   }
 }
 
@@ -21,14 +21,14 @@ trait Stat[T <: Stat[T]] {
   override def toString: String = {
     if (value <= 18) value.toString else s"18/${(value - 18) * 10}"
   }
-  
+
   def toHit: Int = 0
   def toDamage: Int = 0
   def toArmor: Int = 0
 }
 
-class Str(var value: Int) extends Stat[Str] {
-  override def +(bonus: Int): Str = if (value + bonus >= 40) Str(40) else Str(value + bonus)
+class Strength(var value: Int) extends Stat[Strength] {
+  override def +(bonus: Int): Strength = if (value + bonus >= 40) Strength(40) else Strength(value + bonus)
 
   override def toHit: Int = value match {
     case 3 => -3
@@ -53,24 +53,53 @@ class Str(var value: Int) extends Stat[Str] {
     case x if x <= 38 => x - 22
     case 39 => 18
     case _ => 20
-    
   }
 }
 
-class Intg(var value: Int) extends Stat[Intg] {
-  override def +(bonus: Int): Intg = if (value + bonus >= 40) Intg(40) else Intg(value + bonus)
+class Intelligence(var value: Int) extends Stat[Intelligence] {
+  override def +(bonus: Int): Intelligence = if (value + bonus >= 40) Intelligence(40) else Intelligence(value + bonus)
 }
 
-class Wis(var value: Int) extends Stat[Wis] {
-  override def +(bonus: Int): Wis = if (value + bonus >= 40) Wis(40) else Wis(value + bonus)
+class Wisdom(var value: Int) extends Stat[Wisdom] {
+  override def +(bonus: Int): Wisdom = if (value + bonus >= 40) Wisdom(40) else Wisdom(value + bonus)
 }
 
-class Dex(var value: Int) extends Stat[Dex] {
-  override def +(bonus: Int): Dex = if (value + bonus >= 40) Dex(40) else Dex(value + bonus)
+class Dexterity(var value: Int) extends Stat[Dexterity] {
+  override def toHit: Int = value match {
+    case 3 => -3
+    case x if x <= 5 => -2
+    case x if x <= 7 => -1
+    case x if x <= 15 => 0
+    case 16 => 1
+    case 17 => 2
+    case x if x <= 22 => 3
+    case x if x <= 26 => 4
+    case x if x <= 30 => x - 22
+    case x if x <= 32 => 8
+    case x if x <= 37 => x - 23
+    case _ => 15
+  }
+
+  override def toArmor: Int = value match {
+    case 3 => -4
+    case 4 => -3
+    case 5 => -2
+    case 6 => -1
+    case x if x <= 14 => 0
+    case x if x <= 17 => 1
+    case x if x <= 22 => 2
+    case x if x <= 25 => 3
+    case x if x <= 30 => x - 22
+    case x if x <= 32 => 9
+    case x if x <= 37 => x - 23
+    case _ => 15
+  }
+
+  override def +(bonus: Int): Dexterity = if (value + bonus >= 40) Dexterity(40) else Dexterity(value + bonus)
 }
 
-class Con(var value: Int) extends Stat[Con] {
-  override def +(bonus: Int): Con = if (value + bonus >= 40) Con(40) else Con(value + bonus)
+class Constitution(var value: Int) extends Stat[Constitution] {
+  override def +(bonus: Int): Constitution = if (value + bonus >= 40) Constitution(40) else Constitution(value + bonus)
 }
 
 case class StatBonuses(str: Int, intg: Int, wis: Int, dex: Int, con: Int) {
