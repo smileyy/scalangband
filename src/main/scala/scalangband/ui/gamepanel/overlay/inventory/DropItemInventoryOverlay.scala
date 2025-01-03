e12b5e6ccd9be750c34c0fcd21051bb1e4c6f0f8
smileyy <smileyy@gmail.com>
2025-01-03 15:03:53 -0800

    267: stacking message bugs (#272)

--- src/main/scala/scalangband/ui/gamepanel/overlay/inventory/DropItemInventoryOverlay.scala
+++ src/main/scala/scalangband/ui/gamepanel/overlay/inventory/DropItemInventoryOverlay.scala
@@ -5,12 +5,11 @@ import scalangband.model.item.Item
 import scalangband.model.player.action.{DropInventoryItemAction, PlayerAction}
 import scalangband.ui.gamepanel.overlay.ItemActionFactory
 
-
 object DropItemInventoryOverlay {
   def apply(game: Game): InventoryOverlay = {
     new InventoryOverlay(game, "Drop which item?", factory = DropItemActionFactory)
   }
 }
 object DropItemActionFactory extends ItemActionFactory {
-  override def apply(item: Item): Option[PlayerAction] = Some(new DropInventoryItemAction(item, 1))
+  override def apply(item: Item): Option[PlayerAction] = Some(new DropInventoryItemAction(item))
 }
