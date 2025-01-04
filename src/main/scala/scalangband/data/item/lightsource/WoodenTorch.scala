a408347534c768ccf36645741d3634f6078fe5a9
smileyy <smileyy@gmail.com>
2025-01-04 15:49:27 -0800

    220: reduce torch light radius (use wizard mode if you really want to see)

--- src/main/scala/scalangband/data/item/lightsource/WoodenTorch.scala
+++ src/main/scala/scalangband/data/item/lightsource/WoodenTorch.scala
@@ -5,7 +5,7 @@ import scalangband.model.item.lightsource.{WoodenTorchFactory, WoodenTorchSpec}
 
 object WoodenTorch extends WoodenTorchFactory {
   override val spec: WoodenTorchSpec = WoodenTorchSpec(
-    radius = 2, initialTurns = 5000, color = Umber
+    radius = 1, initialTurns = 5000, color = Umber
   )
 
   override val levels: Range = 1 to 100
